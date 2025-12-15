a895738fda8367fa4a59bd3d04ed0417aeccfb76
Chandan Bisht <chandanbisht855@gmail.com>
2025-12-15 12:10:06 +0530

    change.py added

---
+++ Interactive_mode/tokens.py
@@ -0,0 +1,28 @@
+val = 0.05E+2 # scientific notation
+print(val)
+
+String1 = "text"
+print(String1)
+
+String2 = "Text\
+That is spread across\
+multiple lines."
+
+print(String2)
+
+String3 = """Third
+Multiline
+Text"""
+print(String3)
+
+
+print(len(String1))
+print(len(String2))
+print(len(String3))
+
+
+A = None
+print(A)
+
+chk = True
+print(chk)
